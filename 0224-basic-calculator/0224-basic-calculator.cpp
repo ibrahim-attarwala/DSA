5556817e652a7356d453fc765c46dd0883ab6e17
ibrahim-attarwala <ibrahim.attarwala515253@gmail.com>
2026-09-21 10:34:40 +0530

    Time: 3 ms (55.04%), Space: 12.5 MB (83.77%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -4,6 +4,7 @@ public:
         stack<long long> st;
         long long ans=0,n=s.size();
         long long num=0,sign=1;
+        st.push(sign);
         for(int i=0;i<n;i++){
             if(s[i]==' ')continue;
             if(s[i]>='0' && s[i]<='9'){
@@ -11,30 +12,21 @@ public:
             }
             else if(s[i]=='+'){
                 ans=ans+sign*num;
-                sign=1;
+                sign=1*st.top();
                 num=0;
             }
             else if(s[i]=='-'){
                 ans+=sign*num;
-                sign=-1;
+                sign=-1*st.top();
                 num=0;
             }
             else if(s[i]=='('){
-                st.push(ans);
                 st.push(sign);
-                sign=1;
-                ans=0;
             }
             else{
-                ans+=sign*num;
-                num=0;
-                ans*=st.top();
-                st.pop();
-                ans+=st.top();
                 st.pop();
             }
         }
-        ans+=sign*num;
-        return (int)ans;
+        return ans+sign*num;
     }
 };
