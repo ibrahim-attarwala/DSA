ce3310be59a456604dac3c766b52ab50765bf4d2
ibrahim-attarwala <ibrahim.attarwala515253@gmail.com>
2026-09-21 11:07:02 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,8 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int sum=0;
         int n=nums.size();
-        for(int i=1;i<=n;i++)sum^=i;
-        for(int i=0;i<n;i++)sum^=nums[i];
-        return sum;
+        for(int i=0;i<n;i++)sum+=nums[i];
+        return (n*(n+1))/2- sum;
     }
 };
